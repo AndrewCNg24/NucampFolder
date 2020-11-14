c91f1fb55a6249c50ad9966ed4969f18dd7358c0
AndrewCNg24 <ang26@ucmerced.edu>
2020-11-14 12:57:38 -0800

    Week 2 Changes

---
+++ 2-Bootstrap/PenCodePractice/Week2/Exercise2/file.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function(){
+    $("#part1").addClass("notStacked insidePBorder textAlignTop");
+    $("#part11").addClass("notStacked insideMBorder textAlignTop");
+    $("#part2").addClass("notStacked insidePBorder textAlignBottom");
+    $("#part21").addClass("notStacked insideMBorder textAlignBottom");
+    $("#part3").addClass("notStacked insidePBorder2 textAlignRight");
+    $("#part31").addClass("notStacked insideMBorder2 textAlignRight");
+    $("#part4").addClass("notStacked insidePBorder2 textAlignLeft");
+    $("#part41").addClass("notStacked insideMBorder2 textAlignLeft");
+    $("#part5").addClass("notStacked insidePBorder");
+    $("#part51").addClass("notStacked insideMBorder");
+    $("#part6").addClass("notStacked insidePBorder");
+    $("#part61").addClass("notStacked insideMBorder");
+  });

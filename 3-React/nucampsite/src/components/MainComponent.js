b85619bbadc147f414da4a693b0d242c787b8d68
andrewcng24 <andrewc.ng24@gmail.com>
2021-01-01 20:14:36 -0800

    Week 3 Exercise SIngle Page App part 2

--- 3-React/nucampsite/src/components/MainComponent.js
+++ 3-React/nucampsite/src/components/MainComponent.js
@@ -12,6 +12,15 @@ import { COMMENTS } from '../shared/comments';
 import { PARTNERS } from '../shared/partners';
 import { PROMOTIONS } from '../shared/promotions';
 
+const CampsiteWithId = ({match}) => {
+    return (
+        <CampsiteInfo 
+            campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+            comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+        />
+    );
+};    
+
 class Main extends Component {
     constructor(props) {
         super(props);
@@ -23,6 +32,7 @@ class Main extends Component {
         };
     }
 
+    
     render () {
         const HomePage =() => {
             return (
@@ -38,7 +48,7 @@ class Main extends Component {
                 <Header />
                 <Switch>
                     <Route path='/home' component={HomePage} />
-                    <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Route path='/directory/:campsiteID' component={CampsiteWithId} />
                     <Route exact path='/contactus' component={Contact} />
                     <Redirect to='/home' />
                 </Switch>

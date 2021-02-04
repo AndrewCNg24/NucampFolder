552fe0a14f049f41d6ae39a51629d021fa29a8ef
AndrewCNg24 <ang26@ucmerced.edu>
2021-02-04 02:23:55 -0800

    Fixed?

---
+++ 3-React/nucampsite/src/components/CampsiteInfoConponents.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class CampsiteInfo extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+        return (
+            <div>
+                
+            </div>
+        );
+
+    }
+
+}
+export default CampsiteInfo;

13712fa62e2e8c916b2ca6fd14194f31d07eca16
AndrewCNg24 <ang26@ucmerced.edu>
2021-02-04 02:25:02 -0800

    Week 1 React Native

---
+++ 4-React-Native/nucampsite/components/About.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, View, ScrollView } from 'react-native';
+import { Card } from 'react-native-elements';
+import { CAMPSITES } from '../shared/campsites';
+
+class About extends Component {
+    render() {
+        return (
+            <ScrollView></ScrollView>
+        )
+    }
+}
+
+export default About;
---
+++ 4-React-Native/nucampsite/components/Contact.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import { Card } from 'react-native-elements';
+import { CAMPSITES } from '../shared/campsites';
+
+class Contacts extends Component {
+    render() {
+        return (
+            <ScrollView></ScrollView>
+        )
+    }
+}
+
+export default Contact;
